16434d2f4e8b167cc0a9bf9e8c488212092082e2
jason zhang <jasonzhang2002@gmail.com>
2019-05-14 16:04:33 -0700

    add transfer learning examples

---
+++ image/transfer_learning_keras_app_readme.md
@@ -0,0 +1,7 @@
+1. This example uses keras.application instead of hub module. Image caption with attention model also use keras 
+   application to extract feature from image
+1. It uses tfds.dataset
+   1. It demos how one use split.
+   1. How one read meta information from the dataset. 
+   1. Again, it uses tf.image package to process image(cast, resize, feature scaling and normalization)
+
---
+++ image/transfer_learning_with_keras_app.py
@@ -0,0 +1,68 @@
+from __future__ import print_function, absolute_import, unicode_literals,  division
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow.keras as keras
+import tensorflow_datasets as tfds
+
+split_weights=(8,1,1)
+#found this from mobile_net
+IMAGE_SIZE = (224, 224)
+splits = list(tfds.Split.TRAIN.subsplit(weighted=split_weights))
+#buffer size can't be big loadintg so much image in memory uses a lot of resorces
+BUFFER_SIZE = 1000
+BATCH = 32
+(raw_train, raw_validation, raw_test), info = tfds.load("cats_vs_dogs", split=splits, with_info=True, as_supervised=True)
+'''
+image characteristic: each image has its own size.
+the shape is (None, None, 3).
+the dtype is uint8. 
+'''
+num_train_examples = info.splits['train'].num_examples
+
+def process_image(image, label):
+    # This is not necessary. In python3, call math division is float division
+    image = tf.cast(image, tf.float32)
+    image = tf.image.resize(image, size=IMAGE_SIZE)
+    #value is from [-1, 1]
+    #feature scaling and normalization
+    image = image/127.5 -1
+
+    #label is integer, 0, 1
+    # 0 is cat, 1 is dog
+    return image, label
+
+# Map has to be done before batch since image has different size and can't be batched.
+train_ds = raw_train.shuffle(BUFFER_SIZE).map(process_image).batch(BATCH, drop_remainder=True)
+validation_ds = raw_validation.map(process_image).batch(BATCH, drop_remainder=True)
+test_ds = raw_test.map(process_image).batch(BATCH, drop_remainder=True)
+
+# input_shape is default (224, 224, 3)
+# weights is default : imagenet
+feature_extractor = keras.applications.mobilenet_v2.MobileNetV2(include_top=False)
+feature_extractor.trainable = False
+
+model = keras.Sequential([
+    feature_extractor,
+    keras.layers.GlobalAveragePooling2D(),
+    keras.layers.Dense(1, activation="sigmoid")
+])
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+step_per_epoch = num_train_examples*8//10
+initial_epochs = 10
+total_epochs = 20
+model.fit(train_ds, validation_data=validation_ds, steps_per_epoch=step_per_epoch, epochs=initial_epochs)
+
+feature_extractor.trainable = True
+for layer in feature_extractor.layers[:100]:
+    layer.trainable = False
+model.fit(train_ds, validation_data=validation_ds, steps_per_epoch=step_per_epoch, epochs=total_epochs, initial_epoch = initial_epochs)
+print(model.evaluate(validation_ds))
+
+
+
+
+
+
+
